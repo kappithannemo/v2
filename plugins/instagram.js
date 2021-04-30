abed25e2c9c7311d83931aef25693fcc58dc1e3d
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 21:03:11 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-*/
+
 
 
 const Asena = require('../events');
@@ -66,3 +66,4 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, des
   },
 )
 
+*/
