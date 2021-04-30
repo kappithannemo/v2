a6a0369ed432ca58bd365eb113ffab770e7fcf8c
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 21:47:23 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -4,7 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
-*/
+
 
 const Asena = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
@@ -370,3 +370,4 @@ Asena.addCommand({pattern: 'pimg ?(.*)', fromMe: true, dontAddCommandList: true 
     });
 }));
 
+*/
