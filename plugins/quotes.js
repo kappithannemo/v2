3545239451c37f3a83afa43ab972c01311a4e0ac
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 21:43:54 +0530

    Update quotes.js

--- plugins/quotes.js
+++ plugins/quotes.js
@@ -30,16 +30,4 @@ Asena.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC},
 });
 
 
-Asena.addCommand({pattern: 'pquote ?(.*)', fromMe: true, dontAddCommandList: true}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.quotable.io/random`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
-		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
-	}
-});
 
