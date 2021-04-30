584c926e632274c39241fca9172acf7a3f16fd6e
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 20:53:00 +0530

    Update evaluators.js

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -4,7 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
-*/
+
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
@@ -26,3 +26,4 @@ Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, desc: Lang.TERM_DESC, don
         return await message.client.sendMessage(message.jid,'```' + user + ':~# ' + match[1] + '\n' + stdout + '```',MessageType.text);
       });
 }));
+*/
