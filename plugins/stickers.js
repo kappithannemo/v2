3ea3f9e446b812d1eddea053abf66a5559ca84aa
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 21:51:44 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -4,7 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
-*/
+
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
@@ -110,3 +110,4 @@ Asena.addCommand({pattern: 'psticker', fromMe: true  , dontAddCommandList: true 
         });
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
+*/
